e7a7425a47b3ff1e576c81679140fff9bcf1f863
roshaankhan10 <roshaankhan50@gmail.com>
2025-07-23 19:30:42 +0500

    added migrate and set user db and password

--- backend/settings.py
+++ backend/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'khidmat_db',
+        'USER': 'roshaan_khimat',  # not khidmat here but khimar , missed the d
+        'PASSWORD': 'securepass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
